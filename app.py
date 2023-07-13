c93fc97128b6494c05525fc4496f588a67452ef4
Alan Autrand <alan.autrand@gmail.com>
2023-07-13 09:31:53 -0700

    Adding blueprints for pizza and topping api.

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from src.controllers.api import pizza_api, toppings_api
 
 app = Flask(__name__)
+
+# Register blueprints
+app.register_blueprint(toppings_api)
+app.register_blueprint(pizza_api)
 
 
 @app.route("/")
---
+++ src/controllers/api/__init__.py
@@ -0,0 +1,2 @@
+from src.controllers.api.pizza_api_controller import pizza_api
+from src.controllers.api.topping_api_controller import toppings_api
