1bebe7776d0b3c51f811a39b71b4e3e90d914c76
Alan Autrand <alan.autrand@gmail.com>
2023-07-13 11:33:47 -0700

    Implementing toppings service. Updating toppings api controller.

--- src/controllers/api/topping_api_controller.py
+++ src/controllers/api/topping_api_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 from src.extensions import db
 from src.services import ToppingService
@@ -17,19 +17,25 @@ def toppings_list():
 
 @toppings_api.route("/toppings/<int:topping_id>", methods=["GET"])
 def get_topping_by_id(topping_id):
-    return "topping by id"
+    return topping_service.get_topping_by_id(topping_id)
 
 
 @toppings_api.route("/toppings", methods=["POST"])
 def create_topping():
-    return "create topping"
+    data = request.get_json()
+    topping_name = data["name"]
+
+    return topping_service.create_topping(topping_name)
 
 
 @toppings_api.route("/toppings/<int:topping_id>", methods=["PUT"])
 def update_topping(topping_id):
-    return "update topping"
+    data = request.get_json()
+    new_topping_name = data["name"]
+
+    return topping_service.update_topping(topping_id=topping_id, new_topping_name=new_topping_name)
 
 
 @toppings_api.route("/toppings/<int:topping_id>", methods=["DELETE"])
 def delete_topping(topping_id):
-    return "delete topping"
+    return topping_service.delete_topping(topping_id)
--- src/services/topping_service.py
+++ src/services/topping_service.py
@@ -11,6 +11,7 @@ class ToppingService:
 
     @common_response
     def get_toppings_list(self):
+
         toppings_list = self.topping.query.all()
 
         if not toppings_list:
@@ -19,7 +20,9 @@ class ToppingService:
         toppings_json = [topping.to_dict() for topping in self.topping.query.all()]
         return jsonify(toppings=toppings_json)
 
+    @common_response
     def get_topping_by_id(self, topping_id):
+
         topping = self.topping.query.get(topping_id)
 
         if not topping:
@@ -27,11 +30,47 @@ class ToppingService:
 
         return jsonify(toppings=topping.to_dict())
 
-    def create_topping(self):
-        pass
+    @common_response
+    def create_topping(self, topping_name):
 
-    def update_topping(self, topping_id):
-        pass
+        conflicting_topping = self.topping.query.filter(self.topping.name == topping_name).first()
 
+        if conflicting_topping:
+            return jsonify(message=f"Topping with name {topping_name} already exists"), 409
+
+        new_topping = self.topping(name=topping_name)
+
+        self.db.session.add(new_topping)
+        self.db.session.commit()
+
+        return jsonify(topping=new_topping.to_dict())
+
+    @common_response
+    def update_topping(self, topping_id, new_topping_name):
+
+        old_topping = self.topping.query.get(topping_id)
+        if not old_topping:
+            return jsonify(message=f'Topping with id {topping_id} not found'), 404
+
+        conflicting_topping = self.topping.query.filter(self.topping.name == new_topping_name).first()
+        if conflicting_topping:
+            return jsonify(message=f"Topping with name {new_topping_name} already exists"), 409
+
+        old_topping.name = new_topping_name
+
+        self.db.session.commit()
+
+        return jsonify(topping=old_topping.to_dict())
+
+    @common_response
     def delete_topping(self, topping_id):
-        pass
+
+        topping = self.topping.query.get(topping_id)
+
+        if not topping:
+            return jsonify(message=f'Topping with id {topping_id} not found'), 404
+
+        self.db.session.delete(topping)
+        self.db.session.commit()
+
+        return jsonify(message="Topping successfully deleted")
